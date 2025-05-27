98b9b0e59a2738005d2a1f575d2ab8e26d5e828c
Robert-Govard <g0896804254@gmail.com>
2025-05-27 11:11:34 +0300

    TODO: dinamic user id

--- src/main.py
+++ src/main.py
@@ -1,5 +1,7 @@
 import asyncio
 from aiogram import F, Bot, Dispatcher, types, exceptions
+from aiogram.types import Message
+from aiogram.filters import Command
 from loguru import logger
 from redis.asyncio import Redis
 
@@ -19,6 +21,14 @@ redis = Redis(
 )
 EX_TIME = 60 * 60 * 24 * 21  # 21 день
 
+@dp.message(Command("start"))
+async def get_user_id(message: Message):
+    try:
+        await message.answer(f"your user id: {message.from_user.id if message.from_user else None}") 
+        return message.from_user.id if message.from_user else None
+    
+    except Exception as error:
+        logger.error(f"Ошибка в получении user id: {error}")
 
 async def set_message(message: types.Message) -> None:
     """Сохраняет сообщение в Redis с истечением через EX_TIME."""
@@ -32,7 +42,9 @@ async def set_message(message: types.Message) -> None:
 
         def isPhoto(message: types.Message):
             if message.photo:
-                return "Photo", message.from_user.first_name if message.from_user else None
+                return "Photo", (
+                    message.from_user.first_name if message.from_user else None
+                )
             return message.text, (
                 message.from_user.first_name if message.from_user else None
             )
@@ -72,19 +84,17 @@ async def edited_message(new_message: types.Message):
             return
 
         if new_message.photo:
-            await bot.send_message(settings.USER_ID, f"Edited photo by {new_message.from_user.first_name if new_message.from_user else None}:")
+            await bot.send_message(
+                settings.USER_ID,
+                f"Edited photo by {new_message.from_user.first_name if new_message.from_user else None}:",
+            )
             await new_message.send_copy(settings.USER_ID).as_(bot)
         else:
             await bot.send_message(
-                settings.USER_ID,
-                f"Сообщение было изменено\n\nold message: {old_message.text} \nnew message: {new_message.text} \nuser: {new_message.from_user.first_name if new_message.from_user else None}",
+                chat_id=settings.USER_ID,
+                text=f'Сообщение было изменено\n\nold message:\n<blockquote>{old_message.text}</blockquote>new message: <blockquote>{new_message.text}</blockquote> \nuser: {new_message.from_user.first_name if new_message.from_user else None}',
+                parse_mode='HTML'
             )
-
-        # проверка является ли сообщение фотографией, если да, то возвращается лог что это фото
-        def isPhoto(message: types.Message):
-            if message.photo:
-                return "Photo"
-            return message.text
 
         logger.info(
             f"Сообщение было изменено: {new_message.chat.id}:{new_message.message_id}"
@@ -94,7 +104,6 @@ async def edited_message(new_message: types.Message):
         logger.error(f"Ошибка при сохранении измененного сообщения: {error}")
 
 
-# сделать сохранение удалённых сообщений, сделать сохранение фотографий и кружков
 @dp.deleted_business_messages()
 async def deleted_message(business_messages: types.BusinessMessagesDeleted):
     pipe = redis.pipeline()
@@ -112,19 +121,41 @@ async def deleted_message(business_messages: types.BusinessMessagesDeleted):
             continue
 
         if old_message.photo:
-            await bot.send_message(settings.USER_ID, f"Deleted photo:")
+            await bot.send_message(
+                settings.USER_ID,
+                f"Deleted photo by {old_message.from_user.first_name}:",
+            )
             await old_message.send_copy(settings.USER_ID).as_(bot)
-
-        send_copy = bot.send_message(settings.USER_ID, f"Удаленное сообщение: {old_message.text}\nuser: {old_message.from_user.first_name}")
+        if old_message.voice:
+            await bot.send_message(
+                settings.USER_ID,
+                f"Deleted voice message by {old_message.from_user.first_name}:",
+            )
+            await old_message.send_copy(settings.USER_ID).as_(bot)
+        if old_message.video_note:
+            await bot.send_message(
+                settings.USER_ID,
+                f"Deleted video note by {old_message.from_user.first_name}:",
+            )
+            await old_message.send_copy(settings.USER_ID).as_(bot)
+        if old_message.video:
+            await bot.send_message(
+                settings.USER_ID,
+                f"Deleted video by {old_message.from_user.first_name}:",
+            )
+            await old_message.send_copy(settings.USER_ID).as_(bot)
+        if old_message.text:
+            await bot.send_message(
+                settings.USER_ID,
+                f"Удаленное сообщение: {old_message.text}\nuser: {old_message.from_user.first_name}",
+            )
 
         try:
-            await send_copy
             logger.info(f"Сообщение было удалено: {old_message.text}")
         except exceptions.TelegramRetryAfter as exp:
             logger.warning(f"Retry after {exp.retry_after} seconds")
             logger.error(f"Сообщение было удалено, но возникла ошибка: {exp}")
             await asyncio.sleep(exp.retry_after + 0.1)
-            await send_copy
         finally:
             await asyncio.sleep(0.1)
 
@@ -144,7 +175,6 @@ async def close(query: types.CallbackQuery):
     await query.answer()
     if isinstance(query.message, types.Message):
         await query.message.delete()
-
 
 # Run the bot
 async def main() -> None:
